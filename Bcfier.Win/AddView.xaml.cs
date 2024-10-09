d2d2ec34508b126bc88307f347fb2403ae323c6b
Tyan <tyan.evgeniy@gmail.com>
2024-10-09 21:53:45 +0300

    AddView dialog styling for BCFier.Win

---
+++ Bcfier.Win/AddView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Bcfier.Bcf.Bcf2;
+using System.Windows;
+
+namespace Bcfier.Win
+{
+  /// <summary>
+  /// Interaction logic for AddView.xaml
+  /// </summary>
+  public partial class AddView : Window
+  {
+    public AddView(Markup issue, string bcfTempFolder)
+    {
+      this.InitializeComponent();
+      AddViewControl.Issue = issue;
+      AddViewControl.TempFolder = bcfTempFolder;
+    }
+  }
+}
