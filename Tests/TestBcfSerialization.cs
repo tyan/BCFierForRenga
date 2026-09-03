72fbf8e4e099c98822f3ae851ee0ed68b77211cd
tyan <tyan.evgeniy@gmail.com>
2026-09-03 09:19:40 +0300

    Add round-trip integrity tests for BCF deserialization

--- Tests/TestBcfSerialization.cs
+++ Tests/TestBcfSerialization.cs
@@ -43,6 +43,52 @@ namespace Tests
     }
 
     [Test]
+    public void load_bcf_preserves_issue_and_viewpoint_counts()
+    {
+      // given
+      var container = new BcfContainer();
+      container.OpenFile(_noSnapshotBcfPath);
+
+      // when
+      var bcf = container.BcfFiles.Single();
+      var issue = bcf.Issues.Single();
+
+      // then
+      Assert.That(bcf.Issues, Has.Count.EqualTo(1));
+      Assert.That(issue.Viewpoints, Has.Count.EqualTo(1));
+      Assert.That(issue.Comment, Has.Count.EqualTo(0));
+      Assert.That(issue.Topic.Guid, Is.EqualTo("2f0e1b14-3d8f-4a0b-9c61-1b53a27e3c4a"));
+      Assert.That(issue.Topic.Title, Is.EqualTo("Issue without snapshot"));
+    }
+
+    [Test]
+    public void save_and_reload_preserves_issue_title_and_counts()
+    {
+      // given
+      var container = new BcfContainer();
+      container.OpenFile(_noSnapshotBcfPath);
+
+      var bcf = container.BcfFiles.Single();
+      var outputDir = Path.Combine(AppContext.BaseDirectory, "test_temp");
+      Directory.CreateDirectory(outputDir);
+      var outPath = Path.Combine(outputDir, "no_snapshot_out.bcf");
+      bcf.Fullname = outPath;
+
+      // when
+      container.SaveFile(bcf);
+
+      // reload the saved file
+      var reloaded = new BcfContainer();
+      reloaded.OpenFile(outPath);
+      var reloadedIssue = reloaded.BcfFiles.Single().Issues.Single();
+
+      // then
+      Assert.That(reloadedIssue.Topic.Title, Is.EqualTo("Issue without snapshot"));
+      Assert.That(reloadedIssue.Viewpoints, Has.Count.EqualTo(1));
+      Assert.That(reloadedIssue.Comment, Has.Count.EqualTo(0));
+    }
+
+    [Test]
     public void save_bcf_without_snapshot_does_not_throw()
     {
       // given
